a8a4b2d258c11cee7caeaef3673b0c778e715a7e
ashinsabu <ashin.sabu3@gmail.com>
2022-11-22 02:57:55 +0530

    Firebase connected and betting function added

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const placebet = () => {
-    
-}
+import { getDatabase,ref,set } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js'
+
+
+const placebet = (user,betAmt,team) => {
+    const db = getDatabase();
+    set(ref(db,'bets/'+user),{
+        team: team,
+        betAmt: betAmt
+    })
+}   
