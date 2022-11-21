034234fd6bcd3d86e6c8774d7fecaae37b0ae70e
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2022-11-22 04:08:32 +0530

    fix: Toggle Status

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const toggleModal = () => {
 
 document.getElementById("place-bets-btn").onclick = () => {
 	toggleModal();
+	// toggleStatus();
 };
 
 document.getElementById("deactivate-btn").onclick = () => {
@@ -13,4 +14,34 @@ document.getElementById("deactivate-btn").onclick = () => {
 
 document.getElementById("cancel-btn").onclick = () => {
 	toggleModal();
+};
+
+const toggleStatus = () => {
+	const elem = document.getElementById("status");
+	switch (elem.getAttribute("data-status")) {
+		case "open":
+			elem.classList.remove(
+				"bg-green-500",
+				"text-green-400",
+				"border-green-500"
+			);
+			elem.classList.add("bg-red-500", "text-red-400", "border-red-500");
+			elem.setAttribute("data-status", "closed");
+			elem.lastChild.innerText = "Closed";
+			break;
+		case "closed":
+			elem.classList.add(
+				"bg-green-500",
+				"text-green-400",
+				"border-green-500"
+			);
+			elem.classList.remove(
+				"bg-red-500",
+				"text-red-400",
+				"border-red-500"
+			);
+			elem.setAttribute("data-status", "open");
+			elem.lastChild.innerText = "Open";
+			break;
+	}
 };
